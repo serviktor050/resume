28d04e16c7bc29322b9798d09bf30ce8f997ca5d
serviktor050 <mr.s-gagulin@yandex.ru>
2023-06-20 23:33:28 +0300

    Routing is enabled on the project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import {Header, Sidebar} from "../shared/ui";
+import {Route, Routes} from 'react-router-dom'
+import {AboutPage} from "../pages";
 
 function App() {
   return (
-    <div className="App">
-      Чистый лист
-    </div>
+    <>
+      <Header />
+      <div className="main-container">
+        <Sidebar />
+        <div className="content-layout">
+          <Routes>
+            <Route path='/' element={<AboutPage />}/>
+          </Routes>
+        </div>
+      </div>
+    </>
   );
 }
 
