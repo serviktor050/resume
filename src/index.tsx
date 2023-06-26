923fa120faaa163fec4300cc81d2d2b2876a2998
serviktor050 <mr.s-gagulin@yandex.ru>
2023-06-26 23:33:39 +0300

    Multilingual connections using i18next

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/App';
 
+import './i18n';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <React.Suspense fallback="loading">
+        <App />
+      </React.Suspense>
     </HashRouter>
   </React.StrictMode>,
 );
--- src/shared/ui/Header/index.tsx
+++ src/shared/ui/Header/index.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import classNames from 'classnames/bind';
+import { useTranslation } from 'react-i18next';
+
+import styles from './styles.module.css';
+
+const cx = classNames.bind(styles);
 
 function Header() {
+  const { t, i18n } = useTranslation();
+
+  const lngs = {
+    ru: 'Russian',
+    en: 'English',
+  };
+
   return (
-    <div>Header</div>
+    <div className={cx('header')}>
+      <div className={cx('header-title')}>{t('header-title')}</div>
+      <div className={cx('header-navigation')} />
+      <div className={cx('header-translate')} />
+      <div>
+        {Object.keys(lngs).map((lng) => (
+          <button
+            type="submit"
+            key={lng}
+            onClick={() => i18n.changeLanguage(lng)}
+            disabled={i18n.resolvedLanguage === lng}
+          >
+            {lng}
+          </button>
+        ))}
+      </div>
+    </div>
   );
 }
 export default Header;
