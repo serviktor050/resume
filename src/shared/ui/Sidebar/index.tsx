29ca1bea25189488c6e79bb1ef1e450863ddb909
serviktor050 <mr.s-gagulin@yandex.ru>
2023-07-05 00:21:15 +0300

    Added links to the links block in the sidebar

--- src/shared/ui/Sidebar/index.tsx
+++ src/shared/ui/Sidebar/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import classNames from 'classnames/bind';
 import { useTranslation } from 'react-i18next';
 import mainImage from '../../../assets/img/png/main-image.png';
+import github from '../../../assets/img/svg/github.svg';
+import telegram from '../../../assets/img/svg/telegram.svg';
+import facebook from '../../../assets/img/svg/facebook.svg';
 
 import styles from './styles.module.css';
 
@@ -13,7 +16,7 @@ function Sidebar() {
   return (
     <div className={cx('sidebar')}>
       <div className={cx('sidebar-photo')}>
-        <img className={cx('sidebar-photo__img')} alt="" src={mainImage} />
+        <img className={cx('sidebar-photo__img')} alt="mainImage" src={mainImage} />
       </div>
       <div className={cx('sidebar-title')}>
         <div className={cx('sidebar-title__name')}>
@@ -27,7 +30,30 @@ function Sidebar() {
         Описание
       </div>
       <div className={cx('sidebar-links')}>
-        Ссылки
+        <button type="button" className={cx('sidebar-links__icon')}>
+          <a href="https://github.com/serviktor050" target="_blank" rel="noreferrer">
+            <img
+              alt="github"
+              src={github}
+            />
+          </a>
+        </button>
+        <button type="button" className={cx('sidebar-links__icon')}>
+          <a href="https://t.me/ser_viktor" target="_blank" rel="noreferrer">
+            <img
+              alt="telegram"
+              src={telegram}
+            />
+          </a>
+        </button>
+        <button type="button" className={cx('sidebar-links__icon')}>
+          <a href="https://www.facebook.com/profile.php?id=100001830878512" target="_blank" rel="noreferrer">
+            <img
+              alt="facebook"
+              src={facebook}
+            />
+          </a>
+        </button>
       </div>
       <button
         type="submit"
