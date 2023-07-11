073cdbc4498fa7037640bf1bd723aedd1ac7a5d7
serviktor050 <mr.s-gagulin@yandex.ru>
2023-07-11 23:42:32 +0300

    Work has begun on filling the About Me page

--- src/pages/AboutPage/index.tsx
+++ src/pages/AboutPage/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import classNames from 'classnames/bind';
+
+import styles from './styles.module.css';
+
+const cx = classNames.bind(styles);
 
 function AboutPage() {
+  const { t } = useTranslation();
   return (
-    <div>AboutPage</div>
+    <div className={cx('container')}>
+      <div className={cx('title')}>{t('about-page.title')}</div>
+      <div className={cx('wrapper')}>
+        <div className={cx('description')}>{t('about-page.description')}</div>
+      </div>
+    </div>
   );
 }
 export default AboutPage;
